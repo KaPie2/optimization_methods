8e9041f49075a0b8f1abd61f5b2c91aed7464886
Kate_Pirogova <pirogovakata7@gmail.com>
2026-03-24 20:26:36 +0600

    created methods

---
+++ lab1_golden_section.py
@@ -0,0 +1,47 @@
+# вариант 8
+
+import time
+start_time = time.time()
+
+def f(x):
+    return x ** 3 - x
+
+def golden_section(f, a, b, eps):
+    tau = 0.618
+
+    L = b - a
+    x1 = a + L * tau
+    x2 = b - L * tau
+
+    f1 = f(x1)
+    f2 = f(x2)
+
+    while (b - a) > eps:
+        if f1 > f2:
+            b = x1
+            f1 = f2
+            x1 = x2
+            L = b - a
+            x2 = b - L * tau
+            f2 = f(x2)
+        else:
+            a = x2
+            f2 = f1
+            x2 = x1
+            L = b - a
+            x1 = a + L * tau
+            f1 = f(x1)
+
+    x_min, f_min = (x1, f1) if f1 < f2 else (x2, f2)
+
+    # print(f"Ответ:")
+    # print(f"  x_min = {x_min}")
+    # print(f"  f(x_min) = {f_min}")
+
+    return x_min, f_min
+
+# x_min, f_min = golden_section(0, 1, 0.01)
+#
+# end_time = time.time()
+# execution_time = end_time - start_time
+# print(f"Время выполнения: {execution_time:.4f} секунд")
---
+++ lab2_gauss–seidel.py
@@ -0,0 +1,39 @@
+# вариант 8
+
+import math
+from lab1_golden_section import golden_section
+
+def f(x, y):
+    return (30 - (x - 2) * math.exp(-(x - 2)) - (y - 3) * math.exp(-(y - 3)))
+
+def gauss_seidel(x0, eps, max_iter=1000):
+    n = 2
+    x = x0.copy()
+    for k in range(max_iter):
+        x_prev = x.copy()
+        y = x.copy()
+        for j in range(n):
+            a = y[j] - 10
+            b = y[j] + 10
+            if j == 0:
+                y1_fixed = y[1]
+                def f_lambda_x(t):
+                    return f(t, y1_fixed)
+
+                lambda_opt, _ = golden_section(f_lambda_x, a, b, eps)
+                y[0] = lambda_opt
+            else:
+                y0_fixed = y[0]
+                def f_lambda_y(t):
+                    return f(y0_fixed, t)
+
+                lambda_opt, _ = golden_section(f_lambda_y, a, b, eps)
+                y[1] = lambda_opt
+        x = y.copy()
+        diff = math.sqrt((x[0] - x_prev[0])**2 + (x[1] - x_prev[1])**2)
+        if diff < eps:
+            break
+    return x
+
+x_opt = gauss_seidel([0, 0], 1e-5)
+print(f"x1 = {x_opt[0]:.2f}, x2 = {x_opt[1]:.2f}")
